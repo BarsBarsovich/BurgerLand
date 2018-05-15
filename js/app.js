4694bc832a99a6b2c73abad7bba1d11082c6dc56
bars <1462155@mail.ru>
2018-05-15 21:14:14 +0300

    Выпадающее меню

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ $(function () {
   var btn = $('.menu-list__title');
   var activeclass = "menu-list__item_active";
 
+
+  var li = $('.acco__trigger');
+
   btn.click(function (event) {
     event.preventDefault();
     var parent = $(this).parent();
@@ -16,4 +19,35 @@ $(function () {
       parent.addClass(activeclass);
     }
   });
+
+  li.click(function (event){
+    event.preventDefault();    
+    var activeItem = 'acco__item-active';
+
+    var current = $(this).parent();
+    console.log(current);
+    if (current.hasClass(activeItem)){
+      current.removeClass(activeItem)      
+    }else{
+      li.parent().removeClass(activeItem);
+      current.addClass(activeItem);
+    }
+  });
+
+
+  initHamburger();
 });
+
+function initHamburger(){
+  var btn = $('.js-opennav');
+  var activeclass = "nav_active";
+
+  var nav = $('#navid');
+
+  btn.click(function (event) {
+    console.log('click');
+    event.preventDefault();
+    nav.toggleClass(activeclass);
+    $('body, html').toggleClass('active');
+  });
+}
