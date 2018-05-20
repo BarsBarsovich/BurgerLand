7926ba3c2addbfe4cc68f10b6ffda3205ced071a
Boris <boris.dryabin@gmail.com>
2018-05-20 23:44:37 +0300

    OPS+modal window

--- js/app.js
+++ js/app.js
@@ -1,53 +1,101 @@
-$(function () {
-
-  var btn = $('.menu-list__title');
-  var activeclass = "menu-list__item_active";
-
-
-  var li = $('.acco__trigger');
-
-  btn.click(function (event) {
-    event.preventDefault();
-    var parent = $(this).parent();
-
-    if (parent.hasClass(activeclass)) {
-      // есть класс - удаляем
-      parent.removeClass(activeclass);
-    } else {
-      // нет класса показываем этот элемент
-      btn.parent().removeClass(activeclass);
-      parent.addClass(activeclass);
-    }
-  });
-
-  li.click(function (event){
-    event.preventDefault();    
-    var activeItem = 'acco__item-active';
-
-    var current = $(this).parent();
-    console.log(current);
-    if (current.hasClass(activeItem)){
-      current.removeClass(activeItem)      
-    }else{
-      li.parent().removeClass(activeItem);
-      current.addClass(activeItem);
-    }
-  });
-
-
-  initHamburger();
+$(document).ready(function() {
+    $('.maincontent').fullpage({
+        anchors: ['hero', 'best', 'burgers', 'team', 'menu', 'reviews', 'order', 'map'],
+        menu: ".fixed-menu"
+            // scrollOverflow: true
+            // navigationTooltips: ['О нас', '3232', 'rrre']
+    });
 });
 
-function initHamburger(){
-  var btn = $('.js-opennav');
-  var activeclass = "nav_active";
+$(function() {
 
-  var nav = $('#navid');
+    var btn = $('.menu-list__title');
+    var activeclass = "menu-list__item_active";
+    var details = $('.btn-dark');
+    var li = $('.acco__trigger');
+    var modal = $('.modalWindow');
+    var closeModal = $('.close');
 
-  btn.click(function (event) {
-    console.log('click');
-    event.preventDefault();
-    nav.toggleClass(activeclass);
-    $('body, html').toggleClass('active');
-  });
+
+    $('#btn-order').click(function(event) {
+        event.preventDefault();
+        $('.modalOrder').css('display', 'block');
+    });
+
+    $('.btn-close').click(function(event) {
+        event.preventDefault();
+        $('.modalOrder').css('display', 'none');
+    });
+
+    details.click(function(event) {
+        let review_author, review_text;
+        event.preventDefault();
+        let elem_title = event.target.parentElement.getElementsByClassName("reviews__title");
+        if (elem_title.length > 0) {
+            review_author = elem_title[0].innerHTML;
+        }
+
+        let elem_text = event.target.parentElement.getElementsByClassName("reviews__text");
+        if (elem_text.length > 0) {
+            if (elem_text[0].children.length > 0) {
+                review_text = elem_text[0].children[0].innerHTML;
+            }
+        }
+
+        modal.css('display', 'block');
+        $('.title-author-span').html(review_author);
+        $('.modal__text-span').html(review_text);
+
+    });
+
+    closeModal.click(function() {
+        event.preventDefault();
+        modal.css('display', 'none');
+
+    });
+
+    btn.click(function(event) {
+        event.preventDefault();
+        var parent = $(this).parent();
+
+        if (parent.hasClass(activeclass)) {
+            // есть класс - удаляем
+            parent.removeClass(activeclass);
+        } else {
+            // нет класса показываем этот элемент
+            btn.parent().removeClass(activeclass);
+            parent.addClass(activeclass);
+        }
+    });
+
+    li.click(function(event) {
+        event.preventDefault();
+        var activeItem = 'acco__item-active';
+
+        var current = $(this).parent();
+        console.log(current);
+        if (current.hasClass(activeItem)) {
+            current.removeClass(activeItem)
+        } else {
+            li.parent().removeClass(activeItem);
+            current.addClass(activeItem);
+        }
+    });
+
+    initHamburger();
+});
+
+
+function initHamburger() {
+    var btn = $('.js-opennav');
+    var activeclass = "nav_active";
+
+    var nav = $('#navid');
+
+    btn.click(function(event) {
+        console.log('click');
+        event.preventDefault();
+        nav.toggleClass(activeclass);
+        $('body, html').toggleClass('active');
+    });
 }
