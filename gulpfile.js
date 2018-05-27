ec24d431d4eb2c9bc9fdc304faa1689955b6f944
Boris <boris.dryabin@gmail.com>
2018-05-28 01:10:51 +0300

    add gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,29 @@
 'use strict';
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+
+
+var paths = {
+    styles: {
+        src: './css/**/**.scss',
+        build: './dist'
+    }
+};
+gulp.task('scss', function() {
+    return gulp.src(paths.styles.src)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(paths.styles.build));
+});
+
+
+gulp.task('watch', function() {
+    gelp.watch(paths.styles.src, 'scss');
+});
+
+
+gulp.task('default', function() {
+    gulp.watch(paths.styles.src, gulp.series(
+        gulp.parallel('scss'),
+        // gulp.parallel('watch')
+    ));
+});
